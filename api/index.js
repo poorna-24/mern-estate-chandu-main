d3915ad5f6cf9f950bdb013a7ebb5dd94e128983
Poorna Chander Terala <chanduterala24@gmail.com>
2023-12-26 22:46:49 +0530

    Till dotenv file

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ mongoose
   .connect(process.env.MONGO)
 
   .then(() => {
-    console.log("Connected to DB");
+    console.log("Connected to DB!");
   })
   .catch((err) => {
     console.log(err);
