41fd67e3de23006ec82bbbc438f272ab9fa2e2fc
Poorna Chander Terala <chanduterala24@gmail.com>
2023-12-28 07:42:00 +0530

    jwt

--- api/index.js
+++ api/index.js
@@ -23,8 +23,9 @@ app.listen(process.env.PORT, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+//Create a middleware and a function to handle possible errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ message, success: false, statusCode });
+  return res.status(statusCode).json({ success: false, statusCode, message });
 });
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,8 @@
-export const errorHandle = (statusCode, message) => {
+export const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
   return error;
 };
 
-//incse there is no error in system but we need to throw an error ex password length is not enoguh
+//incase there is no error in system but we need to throw an error ex: password length is not enoguh
