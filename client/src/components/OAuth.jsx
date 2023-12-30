e949ab81836697f026c7bdbdbc4f44c128c6b807
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2023-12-31 01:10:22 +0530

    google func completed

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -4,8 +4,11 @@ import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
 
+import { useNavigate } from "react-router-dom";
+
 const OAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -21,6 +24,7 @@ const OAuth = () => {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       console.log("could not signin from google", error);
     }
