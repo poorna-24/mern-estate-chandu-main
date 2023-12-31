dd8d0d3602da89fcb05a3f3f13b06548ec757e51
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-01-01 03:27:20 +0530

    about updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -7,7 +7,7 @@ const About = () => {
         <div className="md:flex md:items-center md:justify-center md:space-x-14">
           <div className="relative h-48 w-48 flex-shrink-0 ">
             <img
-              className="relative h-48 w-48 rounded-full object-cover "
+              className="relative h-48 w-48 rounded-full object-cover hover:pointer-cursor"
               src="https://firebasestorage.googleapis.com/v0/b/mern-estate-fc99f.appspot.com/o/1704055274011chandu1.jpg?alt=media&token=7e83ba4e-cc6a-4513-bd52-4c5e298e35dbhttps://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
               alt=""
             />
@@ -21,9 +21,8 @@ const About = () => {
               </p>
             </blockquote>
             <p className="mt-7 text-lg font-semibold text-black">Poorna Chander Terala</p>
-            <Link src="">
-              <p className="mt-1 text-base text-gray-600">MERN Stack Developer | Full Stack Developer | ReactJs Developer at Tata Consultancy Services</p>
-            </Link>
+
+            <p className="mt-1 text-base text-gray-600">MERN Stack Developer | Full Stack Developer | ReactJs Developer at Tata Consultancy Services</p>
           </div>
         </div>
       </div>
