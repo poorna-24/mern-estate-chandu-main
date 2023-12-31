0eca18725a230c80417007424b12afacd5a05ab6
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-01-01 02:06:11 +0530

    all details update page

--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { test } from "../controllers/user-controller.js";
+import { deleteUser, getUser, test, updateUser } from "../controllers/user-controller.js";
+import { verifyToken } from "../utils/verifyuser.js";
 
 const router = express.Router();
 router.get("/test", test);
-router.post("/");
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+// router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
+
 export default router;
